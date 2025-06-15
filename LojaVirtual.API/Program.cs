4c1205232e1823b16e1107ff1c83473786049e25
Administrador <Administrador@DESKTOP-2FFVM97>
2025-06-15 09:47:49 -0300

    Troca do Datadog pelo Elastic

--- LojaVirtual.API/Program.cs
+++ LojaVirtual.API/Program.cs
@@ -2,13 +2,14 @@ using LojaVirtual.API.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddAllElasticApm();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<DbContextMySql>();
 builder.Services.AddScoped<DbContextMongoDB>();
+
 
 var app = builder.Build();
 
